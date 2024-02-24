ecf25ee19f8dd1d1eb0daa64e0510a7e52aef9ac
zhen <hezhen20@foxmail.com>
2024-02-24 16:46:41 +0800

    feat: add component button

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,30 @@
+<template>
+ <button
+    class="hz-button"
+    :class="{
+      [`hz-button--${type}`]: type,
+      [`hz-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+    }"
+    :disabled="disabled"
+ >
+  <span>
+    <slot />
+  </span>
+ </button>
+</template>
+
+<script setup lang="ts">
+import type { ButtonProps } from './types'
+
+defineProps<ButtonProps>()
+defineOptions({
+  name: 'HzButton'
+})
+</script>
+
+<style scoped>
+</style>
