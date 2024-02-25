013d8ea6d71f0f1d58fbadd3504672bf119c437f
zhen <hezhen20@foxmail.com>
2024-02-25 13:19:44 +0800

    feat: add button native attributes

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -1,5 +1,6 @@
 <template>
  <button
+    ref="_ref"
     class="hz-button"
     :class="{
       [`hz-button--${type}`]: type,
@@ -10,6 +11,8 @@
       'is-disabled': disabled,
     }"
     :disabled="disabled"
+    :autofocus="autofocus"
+    :type="nativeType"
  >
   <span>
     <slot />
@@ -18,11 +21,22 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import type { ButtonProps } from './types'
 
-defineProps<ButtonProps>()
+withDefaults(defineProps<ButtonProps>(), {
+  nativeType: 'button'
+})
+
 defineOptions({
   name: 'HzButton'
+})
+
+const _ref = ref<HTMLButtonElement>()
+
+// 暴露组件实例
+defineExpose({
+  ref: _ref
 })
 </script>
 
--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,6 +1,7 @@
 // import { PropType } from 'vue'
 export type ButtonType = 'primary' | 'success' | 'warning' | 'danger' | 'info'
 export type ButtonSize = 'large' | 'small'
+export type NativeType = 'button' | 'submit' | 'reset'
 
 export interface ButtonProps {
   type?: ButtonType;
@@ -9,4 +10,10 @@ export interface ButtonProps {
   round?: boolean;
   circle?: boolean;
   disabled?: boolean;
+  nativeType?: NativeType;
+  autofocus?: boolean;
+}
+
+export interface ButtonInstance {
+  ref: HTMLButtonElement;
 }
