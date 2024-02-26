bb537a7871a6676434719d323ff2bb932d4deb2c
zhen <hezhen20@foxmail.com>
2024-02-26 15:08:18 +0800

    feat: collapse open and close

--- src/components/Collapse/Collapse.vue
+++ src/components/Collapse/Collapse.vue
@@ -5,7 +5,26 @@
 </template>
 
 <script setup lang="ts">
+import { provide, ref } from 'vue'
+import type { NameType } from './types'
+import { collapseContextKey } from './types'
 defineOptions({
   name: 'HzCollapse'
+})
+const activeNames = ref<NameType[]>([])
+const handleItemClick = (name: NameType) => {
+  const index = activeNames.value.indexOf(name)
+  if (index > -1) {
+    // 存在，要删除，即 CollapseItem 要隐藏
+    activeNames.value.splice(index, 1)
+  } else {
+    // 不存在，要添加，即 CollapseItem 展示
+    activeNames.value.push(name)
+  }
+}
+
+provide(collapseContextKey, {
+  activeNames,
+  handleItemClick
 })
 </script>
--- src/components/Collapse/CollapseItem.vue
+++ src/components/Collapse/CollapseItem.vue
@@ -4,20 +4,30 @@
       'is-disabled': disabled
     }"
   >
-    <div class="hz-collapse-item__header" :id="`item-header-${name}`">
+    <div class="hz-collapse-item__header" :id="`item-header-${name}`" @click="handleClick">
       <slot name="title">{{ title }}</slot>
     </div>
-    <div class="hz-collapse-item__content" :id="`item-content-${name}`">
+    <div class="hz-collapse-item__content" :id="`item-content-${name}`" v-show="isActive">
       <slot />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { CollapseItemProps } from './types'
+import { inject, computed } from 'vue'
+import type { CollapseItemProps } from './types'
+import { collapseContextKey } from './types'
 
 defineOptions({
   name: 'HzCollapseItem'
 })
-defineProps<CollapseItemProps>()
+const props = defineProps<CollapseItemProps>()
+const collapseContext = inject(collapseContextKey)
+const isActive = computed(() => collapseContext?.activeNames.value.includes(props.name))
+const handleClick = () => {
+  if (props.disabled) {
+    return
+  }
+  collapseContext?.handleItemClick(props.name)
+}
 </script>
