c642ec4d84899104a7694d8fa2849bccf1311ce3
zhen <hezhen20@foxmail.com>
2024-03-04 19:52:27 +0800

    feat: add vitepress-demo-preview

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,9 +1,24 @@
 import { defineConfig } from 'vitepress'
+import { containerPreview, componentPreview } from '@vitepress-demo-preview/plugin'
+import { fileURLToPath } from 'url'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "H Element",
   description: "A Vue3 UI Framework",
+  vite: {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('../../src', import.meta.url))
+      }
+    }
+  },
+  markdown: {
+    config(md) {
+        md.use(containerPreview)
+        md.use(componentPreview)
+    },
+  },
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,17 @@
 import DefaultTheme from "vitepress/theme"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import { ElementPlusContainer } from '@vitepress-demo-preview/component'
 
+import '@vitepress-demo-preview/component/dist/style.css'
 import '../../../src/styles/index.css'
 
 library.add(fas)
 
-// 添加一些自定义样式就导出，对 DefaultTheme 不做修改
-export default DefaultTheme
+// 扩展 DefaultTheme
+export default {
+  ...DefaultTheme,
+  enhanceApp({ app }) {
+    app.component('demo-preview', ElementPlusContainer)
+  }
+}
